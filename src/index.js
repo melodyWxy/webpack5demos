0f5785c5ea5b5a88f046df89a773ed4f670c380e
melodyWxy <18210711176@163.com>
2021-10-30 02:16:46 +0800

    test:4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './other'
 import "./styles.css";
 import "./styles.less";
-console.log("hello world!2");
-console.log( 'xixi11123111');
+// console.log("hello world!2");
+// console.log( 'xixi111223311123');
